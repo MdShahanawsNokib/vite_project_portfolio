258d204699755286e69e67d2d4bc9269e2fc9741
MdShahanawsNokib <shahanawsnokib123@gmail.com>
2025-07-05 00:10:05 +0600

    set up theam

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 export const Home = () => {
-    return <div className= "min-h-screen ">
+    return <div className="min-h-screen ">
         <h2 style={{ color: "red" }}>Home</h2>
-
-{/* {1st way to backgorund */}
+        {/* {1st way to backgorund */}
     </div>;
 }
